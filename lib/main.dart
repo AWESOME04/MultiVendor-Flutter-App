1c00b567ba0b62ac6a51673dde5f032c8840fadd
Evans Acheampong <evansachie01@gmail.com>
2025-02-02 17:54:41 +0000

    feat: add product functionality for sellers

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/splash_screen.dart';
+import 'screens/auth_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/cart_screen.dart';
 import 'services/user_service.dart';
+import 'services/cart_notifier.dart';
 
 void main() async {
   // This is required to use platform channels before runApp
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Create and initialize UserService
+  // Initialize UserService
   final userService = UserService();
   await userService.initializeFromStorage();
 
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => userService,
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: userService),
+        ChangeNotifierProvider(create: (_) => CartNotifier()),
+      ],
       child: const MyApp(),
     ),
   );
@@ -28,10 +35,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Multi Vendor App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/auth': (context) => const AuthScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/cart': (context) => const CartScreen(),
+      },
     );
   }
 }
